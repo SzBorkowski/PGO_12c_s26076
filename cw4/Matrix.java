16f55e030e872fd87909535c47709ebfa9f266f9
SzBorkowski <s26076@pjwstk.edu.pl>
2022-05-20 23:44:13 +0200

    cw4

--- cw4/Matrix.java
+++ cw4/Matrix.java
@@ -5,30 +5,51 @@ import java.util.Arrays;
 
 public class Matrix {
 
-    private int[][] tempMatrix;
+    private static int[][] tempMatrix;
     private int[][] mainMatrix;
 
-    private Matrix(int[][] tempMatrix, int[][] mainMatrix) {
-        this.tempMatrix = tempMatrix;
+    public Matrix(int[][] mainMatrix) {
         this.mainMatrix = mainMatrix;
     }
 
-    public static void SetUpMatrix(int rowCount, int columnCount) {
+    public int[][] getTempMatrix() {
+        return tempMatrix;
+    }
 
+    public void setTempMatrix(int[][] tempMatrix) {
+        this.tempMatrix = tempMatrix;
+    }
+
+    public int[][] getMainMatrix() {
+        return mainMatrix;
+    }
+
+    public void setMainMatrix(int[][] mainMatrix) {
+        this.mainMatrix = mainMatrix;
+    }
+
+    public static void setUpMatrix(int rowCount, int columnCount) {
+        if (rowCount <= 0 || columnCount <= 0) {
+            throw new RuntimeException("Incorrect matrix size");
+        } else {
+            tempMatrix = new int[rowCount][columnCount];
+        }
     }
 
     public static void insertRow(int[] row) {
 
     }
 
-    public static Matrix Create() {
-
+    public static Matrix create() {
+        Matrix mainMatrix = new Matrix(tempMatrix);
+        tempMatrix = null;
+        return mainMatrix;
     }
 
     public void print() {
-        for (int i = 0; i < tempMatrix.length; i++) {
-            for (int j = 0; j < tempMatrix.length; j++) {
-                System.out.print(tempMatrix[i][j]);
+        for (int i = 0; i < mainMatrix.length; i++) {
+            for (int j = 0; j < mainMatrix.length; j++) {
+                System.out.print(mainMatrix[i][j]);
             }
             System.out.println();
         }
